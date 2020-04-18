14a87964e16cd6c80a02ed6f83a4410496b413c6
Miles Smith <milessmith1809@gmail.com>
2020-04-18 03:15:28 -0700

    Create PredPreyFuncs.py
    -added the python file that containts the model for the pred prey relationship

---
+++ PredPreyFuncs.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 22:36:40 2020
+
+@author: miles 
+
+dPrey/dt = bPrey - h * Prey * Pred 
+dPred/dt =  eh * Pred * Prey -  dPrey 
+
+prey and predators 
+prams 
+
+b = prey birth rate   
+h = prey/pred interaction rate 
+e = rate if prey population desnity to pred population density
+d = predator death rate 
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def dxydt(xy, b, h, e, d):
+    x, y = xy
+    dx = b * x - h * x * y
+    dy = e * h * x * y - d * y
+    return np.array([dx, dy])
+
+b = 1
+h = 0.005
+e = 0.8
+d = 0.6
+
+steps = 100000
+xy = np.zeros((2, steps))
+xy[:,0] = 20, 100
+dt = 0.001
+
+for t_ in range(1, steps):
+    xy[:,t_] = xy[:, t_-1] + dxydt( xy[:, t_-1], b, h, e, d) * dt
+x = xy[0, :]
+y = xy[1, :]
+
+t = np.arange(0, dt*steps, dt)
+
+plt.plot( t,x, label='prey')
+plt.plot(t, y, label='predator')
+plt.xlabel('Time')
+plt.ylabel('Count')
+plt.legend();
+
+
+ 
