f35165161f784840b63989c9b839d86ea444e6eb
Jacob W. McHugh <31633544+jakemchugh1@users.noreply.github.com>
2020-04-19 12:57:11 -0700

    Audio in game!

---
+++ LD46 ocean game/Assets/MusicPlaylist.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlaylist : MonoBehaviour
+{
+    public bool shuffle;
+    public List<AudioClip> MusicList;
+    public AudioSource source;
+    public int index;
+
+    void start()
+    {
+        if (shuffle)
+        {
+            source.clip = MusicList[(int)Random.Range(0,MusicList.Capacity)];
+        }
+        else
+        {
+            source.clip = MusicList[0];
+        }
+        source.Play();
+
+        
+    }
+
+    void update()
+    {
+        if (!source.isPlaying)
+        {
+            if (!shuffle)
+            {
+                index++;
+                if (index == MusicList.Capacity)
+                {
+                    index = 0;
+                }
+            }
+            else
+            {
+                index = Random.Range(0, MusicList.Capacity);
+            }
+
+            source.clip = MusicList[index];
+            source.Play();
+        }
+    }
+}
+
