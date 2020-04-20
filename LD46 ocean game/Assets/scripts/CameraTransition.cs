d508d228141d600e4d6ed84fc0ea7b0b1f557c48
Miles Smith <milessmith1809@gmail.com>
2020-04-20 03:22:50 -0700

    basic camera motion setup for intro + main menu

--- LD46 ocean game/Assets/scripts/CameraTransition.cs
+++ LD46 ocean game/Assets/scripts/CameraTransition.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class CameraTransition : MonoBehaviour
 {
+    public Vector3 newPostion;
+    public Quaternion newRotation; 
+    public float Speed = 2.0f;
     public void ChangePostion()
     {
-
+        transform.position = Vector3.Lerp(transform.position, newPostion, Speed * Time.deltaTime);
     }
     public void ChangeRotation()
     {
-
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 1.0f * Time.deltaTime);
+    }
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            ChangePostion();
+            if (transform.position == newPostion)
+            {
+                ChangeRotation();
+            }
+        }
     }
 }
