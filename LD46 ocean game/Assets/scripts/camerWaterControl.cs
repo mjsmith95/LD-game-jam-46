9f715e9699793876dc3ede2727ec6b4b2aba79ac
Miles Smith <milessmith1809@gmail.com>
2020-04-20 14:50:34 -0700

    finished a watershader and some ui stuff

---
+++ LD46 ocean game/Assets/scripts/camerWaterControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class camerWaterControl : MonoBehaviour
+{
+    private Camera cam;
+
+
+    // Update is called once per frame
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.depthTextureMode = DepthTextureMode.Depth;
+    }
+}
