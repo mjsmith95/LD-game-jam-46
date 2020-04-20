7e458da058e2b6008c7581d8b7bb9084ba811f36
Jacob W. McHugh <31633544+jakemchugh1@users.noreply.github.com>
2020-04-20 12:48:29 -0700

    after bleaching incident

---
+++ LD46 ocean game/Assets/DisasterSimulator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisasterSimulator : MonoBehaviour
+{
+    public List<Material> coralMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < coralMaterial.Capacity; i++)
+        {
+            coralMaterial[i].color = new Vector4(1, 1, 1,1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
