bdb275d5b97e75803c388186768551ca65d5c097
Jacob W. McHugh <31633544+jakemchugh1@users.noreply.github.com>
2020-04-19 21:43:12 -0700

    Integrating ui

--- LD46 ocean game/Assets/scripts/WindowGraph.cs
+++ LD46 ocean game/Assets/scripts/WindowGraph.cs
@@ -8,37 +8,27 @@ public class WindowGraph : MonoBehaviour
     [SerializeField] private Sprite circleSprite;
     private RectTransform graphContainer;
 
+    public List<GameObject> plotList;
+    public int plotMax;
+
     private void Awake()
     {
-        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
-        CreateCircle(new Vector2(200, 200));
-        List<int> valueList = new List<int>() { 5, 98, 56, 45, 30, 22, 17, 15, 13, 17, 25, 37, 40, 36, 33 };
-        ShowGraph(valueList);
-    }
-
-    private void CreateCircle(Vector2 anchoredPosition)
-    {
-        GameObject gameObject = new GameObject("circle", typeof(Image));
-        gameObject.transform.SetParent(graphContainer, false);
-        gameObject.GetComponent<Image>().sprite = circleSprite;
-        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = anchoredPosition;
-        rectTransform.sizeDelta = new Vector2(11, 11);
-        rectTransform.anchorMin = new Vector2(0, 0);
-        rectTransform.anchorMax = new Vector2(0, 0);
-
-    }
-
-    private void ShowGraph(List<int> valueList)
-    {
-        float graphHeight = graphContainer.sizeDelta.y;
-        float yMaximum = 50f;
-        float xSize = 5f;
-        for(int i = 0; i < valueList.Count ; i++)
+        plotList = new List<GameObject>(plotMax);
+        for (int i = 0; i < plotList.Capacity; i++)
         {
-            float xPosition = i * xSize;
-            float yPosition = (valueList[i] / yMaximum) * graphHeight;
-            CreateCircle(new Vector2(xPosition,yPosition));
+           // plotList[i] = Instantiate<GameObject>(new GameObject("plot", typeof(Image));
         }
+    }
+
+
+    private void updateGraph(int newValue)
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
     }
 }
