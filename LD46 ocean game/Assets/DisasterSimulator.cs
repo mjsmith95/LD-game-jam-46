7c07b327538166fc8364be502ab67da6279e4afc
Jacob W. McHugh <31633544+jakemchugh1@users.noreply.github.com>
2020-04-20 14:40:01 -0700

    Disasters exist!

--- LD46 ocean game/Assets/DisasterSimulator.cs
+++ LD46 ocean game/Assets/DisasterSimulator.cs
@@ -1,22 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DisasterSimulator : MonoBehaviour
 {
-    public List<Material> coralMaterial;
+    public TextMeshProUGUI ticker;
+    public Simulator simulator;
+    public FaunaSpawna faunaSpawna;
+
+    public int prevWholeSecond;
+    public int secBetweenDisasterChance;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < coralMaterial.Capacity; i++)
-        {
-           // coralMaterial[i].color = new Vector4(1, 1, 1,1);
-        }
+        changeTicker("");
+        prevWholeSecond = faunaSpawna.currentWholeSecond;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(prevWholeSecond + secBetweenDisasterChance == faunaSpawna.currentWholeSecond)
+        {
+            int randomEvent = Random.Range(0, 10);
+            simulator.resetToDefault();
+            if(randomEvent < 7)
+            {
+                Stable();
+            }
+            else
+            {
+                randomEvent = Random.Range(0, 3);
+                if(randomEvent == 0)
+                {
+                    Pollution();
+                }else if (randomEvent == 1)
+                {
+                    OverFishing();
+                }
+                else if (randomEvent == 2)
+                {
+                    CoralBleaching();
+                }
+            }
+            prevWholeSecond = faunaSpawna.currentWholeSecond;
+        }
+    }
+
+    public void changeTicker(string newText)
+    {
+        ticker.text = newText;
+    }
+
+    void Stable()
+    {
+        changeTicker("");
+    }
+
+    void Pollution()
+    {
+        changeTicker("Pollution: Predator death rate increase");
+        simulator.ChanngePredDeathRate(0.8f);
+    }
+
+    void OverFishing()
+    {
+        changeTicker("Over Fishing: Fish birthrate decrease");
+        simulator.ChanngePreyBirthRate(0.2f);
+    }
+
+    void CoralBleaching()
+    {
+        changeTicker("Coral Bleaching: 20% Population dies");
+        simulator.predPreyPop[0] =(int)((float)simulator.predPreyPop[0] * 0.8);
+        simulator.predPreyPop[1] = (int)((float)simulator.predPreyPop[1] * 0.8);
+        simulator.Simulate(simulator.predPreyPop);
     }
 }
